217e4ba6a36adb79ef55e0c23485fc8269cb7b18
SOLKIONA <solkiona@gmail.com>
2024-11-27 17:26:02 +0100

    resized most of the large images

--- frontend/app/vendor/page.jsx
+++ frontend/app/vendor/page.jsx
@@ -102,8 +102,6 @@ alt="apple icon"
           // sizes="(max-width: 768px) 100vw,
           //  (max-width: 1200px) 50vw,
           //  33vw"
-          priority // Ensures this image loads faster
-          
   
         />
     </picture>
